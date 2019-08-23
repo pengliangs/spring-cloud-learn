59672b46671487ddc35b63a159fdde07c4895dfa
pengliang <1427985322@qq.com>
2019-08-23 18:28:10 +0800

    添加user-service

---
+++ services/user-service/src/main/java/com/github/pengliangs/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.github.pengliangs.user;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: pengliang
+ * @date: 2019/8/23 18:21
+ * @description：用户服务
+ */
+
+@SpringBootApplication
+public class UserApplication {
+}
