d28feb4f47062b6c6ab9b087aa4b120ee9dcf4e3
pengliang <1427985322@qq.com>
2019-08-23 17:55:00 +0800

    init

---
+++ eureka/src/main/java/com/github/pengliangs/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.github.pengliangs.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author pengliang
+ * @date 2019/08/23 17:16
+ * @description：微服务注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ gateway/src/main/java/com/github/pengliangs/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.github.pengliangs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: pengliang
+ * @date: 2019/8/23 17:27
+ * @description：微服务网关
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
